f6350bca39df447ceb767bff4b2ae50c28fc10c7
Alan Hoffmann <alanzhoffmann@gmail.com>
2023-03-11 03:17:21 +0000

    Add project files.

---
+++ PongzeraSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Raylib_cs;
+
+Console.WriteLine("Hello, World!");
+
+Raylib.InitWindow(800, 600, "Test");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.RAYWHITE);
+
+    Raylib.DrawText("Hello C# Window", 10, 10, 20, Color.BLACK);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
